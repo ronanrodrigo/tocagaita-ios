c6214604530f45556bbe4d728505167679ccc847
Ronan Rodrigo Nunes <ronan.nunes@concrete.com.br>
2017-08-29 09:08:50 -0300

    Implement subtract logic

--- Togaita/TogaitaTests/MissingReachCurrentDayLimit.swift
+++ Togaita/TogaitaTests/MissingReachCurrentDayLimit.swift
@@ -1,59 +1,37 @@
-//
-//  MissingReachCurrentDayLimit.swift
-//  Togaita
-//
-//  Created by Vinicius Carvalho Marques on 27/08/17.
-//  Copyright © 2017 Ronan R. Nunes. All rights reserved.
-//
-
 import XCTest
 @testable import Togaita
 
-protocol CostGateway {
-    func cost() -> [Salary]
+enum TocagaitaError: Error {
+    case costInvalid
 }
 
-struct Cost {
-    let amountDay: Float
-    let amount: Int
-}
-
-struct MissingReachCurrentDayLimit {
-    
-    private let gateway: CostGateway
-    
-    init(gateway: CostGateway) {
-        self.gateway = gateway
+class MissingReachCurrentDayLimit {
+    func calculate(dailyExpense: Float, currentCost: Float) throws -> Float {
+        if currentCost < 0 {
+            throw TocagaitaError.costInvalid
+        }
+        return dailyExpense - currentCost
     }
-    
-    
-    func calculateReportMissingToReach() -> Int {
-        let costForLimit = gateway.cost()
-        
-        let reachedLimit = Salary.amount - costForLimit
-        
-        return reachedLimit
-    }
-    
 }
 
 
 final class MissingReachCurrentDayLimitTests: XCTestCase {
     
     private var sut: MissingReachCurrentDayLimit!
-    private var gateway: CostGatewayMock!
     
     override func setUp() {
-        gateway = CostGatewayMock()
-        sut = MissingReachCurrentDayLimit(gateway: gateway)
+        sut = MissingReachCurrentDayLimit()
         super.setUp()
     }
     
-    func testCalculateWhenReachedLimitThenCalculatePerDayBasedOnSalary() {
-        let limitPerDay = sut.calculateReportMissingToReach
-        
-        //let dailyExpenses = sut.calculate(monthNumber: -1)
-        XCTAssertEqual()
+    func testCalculateWhenCostsThenReturnDailyRemainingLimit() {
+        let limitPerDay = try! sut.calculate(dailyExpense: 100.0, currentCost: 50.0)
+
+        XCTAssertEqual(limitPerDay, 50.0)
+    }
+
+    func testCalculateWhenNegativeCostsThenReturnAnError() {
+        XCTAssertThrowsError(try sut.calculate(dailyExpense: 100.0, currentCost: -50.0))
     }
     
 }
